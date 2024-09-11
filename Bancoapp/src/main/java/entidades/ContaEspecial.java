1720a068081515354e5f5c85e1432a37bda05122
Gasparsk <143566536+Gasparsk@users.noreply.github.com>
2024-09-10 22:53:52 -0300

    Update ContaEspecial.java

--- Bancoapp/src/main/java/entidades/ContaEspecial.java
+++ Bancoapp/src/main/java/entidades/ContaEspecial.java
@@ -4,30 +4,26 @@ public class ContaEspecial extends Conta {
 
     private double limiteEmprestimo;
 
-    // Construtor com depósito inicial e limite de empréstimo
     public ContaEspecial(int numero, String titular, double inicialDeposito, double limiteEmprestimo) {
         super(numero, titular, inicialDeposito);
         this.limiteEmprestimo = limiteEmprestimo;
     }
 
-    // Construtor sem depósito inicial, apenas com limite de empréstimo
     public ContaEspecial(int numero, String titular, double limiteEmprestimo) {
         super(numero, titular);
         this.limiteEmprestimo = limiteEmprestimo;
     }
 
-    // Método para realizar empréstimo
     public void emprestimo(double valor) {
         double taxa = 10.0;
         if (valor <= limiteEmprestimo) {
             limiteEmprestimo -= valor;
             deposito(valor - taxa);
         } else {
-            System.out.println("Empréstimo não aprovado. Valor excede o limite de empréstimo.");
+            System.out.println("Empréstimo não aprovado. Valor ultrapassa o limite de empréstimo.");
         }
     }
 
-    // Método para obter o limite de empréstimo
     public double getLimiteEmprestimo() {
         return limiteEmprestimo;
     }
